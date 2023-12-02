5a6c86c8b0186980cc4f7ddf2c4bbb52e65473d4
zweatshirt <45150570+zweatshirt@users.noreply.github.com>
2023-12-01 23:58:14 -0600

    updates

--- main.py
+++ main.py
@@ -22,9 +22,7 @@ def remove_strongs(word: str) -> (str, str):
 
 
 # turn into more complex nested list bible -> book -> chapter -> verse -> words
-def bible_to_dict(bible: str) -> {}:
-    strip = string.punctuation.replace('{', '').replace('}', '')
-    cleaned_words = bible.lower().translate(str.maketrans('', '', strip)).split()
+def bible_to_dict(bible_lst: []) -> {}:
 
     # bible_list = [[[]]]  # books -> chapters -> verses
     bible_dict = {}
@@ -33,20 +31,20 @@ def bible_to_dict(bible: str) -> {}:
     verse_num = None
     verse = []
 
-    len_words = len(cleaned_words)
+    len_words = len(bible_lst)
 
-    for i, word in enumerate(cleaned_words):
-        if word == 'chapter' or word == 'psalm' and cleaned_words[i + 1].isdigit():
+    for i, word in enumerate(bible_lst):
+        if word == 'chapter' or word == 'psalm' and bible_lst[i + 1].isdigit():
 
             # reset verse for every new chapter
             verse = []
 
             # get chapter name
-            chapter = int(cleaned_words[i + 1])
+            chapter = int(bible_lst[i + 1])
 
             # if chapter name is 1, we can grab the book name one index behind
             if chapter == 1:
-                book = cleaned_words[i - 1]
+                book = bible_lst[i - 1]
 
                 # add book to bible_dict
                 if book and book not in bible_dict:
@@ -63,7 +61,7 @@ def bible_to_dict(bible: str) -> {}:
             break
 
         # look at anything that isn't a chapter, psalm, or book title
-        if ((word != 'chapter' or word != 'psalm') and not cleaned_words[i + 1].isdigit()) and cleaned_words[
+        if ((word != 'chapter' or word != 'psalm') and not bible_lst[i + 1].isdigit()) and bible_lst[
             i - 1] != book and i != 0:
 
             # check for Strongs, save, and remove from word
@@ -83,35 +81,37 @@ def bible_to_dict(bible: str) -> {}:
 
     return bible_dict
 
-    # change this to search a "words to strong dict" and "word counter" dict of the overall bible list
-    # def words_to_dict(words_list: []) -> {}:
-    #
-    #     words_dict = {}
-    #
-    #     for word in words_list:
-    #         strongs = None
-    #         # case only if strong's concordance
-    #         if '{' in word:
-    #             strongs = tuple(re.findall('\{.*?\}', word))
-    #             word = re.sub('\{.*?\}', '', word)
-    #
-    #         if word not in words_dict:
-    #             words_dict[word] = [1, {}]
-    #         else:
-    #             words_dict[word][0] += 1
-    #
-    #         if strongs:
-    #             for s in strongs:
-    #                 if s not in words_dict[word][1]:
-    #                     words_dict[word][1][s] = 1
-    #                 elif s in words_dict[word][1]:
-    #                     words_dict[word][1][s] += 1
-    #     return words_dict
+
+# change this to search a "words to strong dict" and "word counter" dict of the overall bible list
+def bible_to_dict_two(bible_lst: []) -> {}:
+
+    words_dict = {}
+
+    for word in bible_lst:
+        # case only if strong's concordance
+        word, strongs = remove_strongs(word)
+
+        if word not in words_dict:
+            words_dict[word] = [1, {}]
+        else:
+            words_dict[word][0] += 1
+
+        if strongs:
+            for s in strongs:
+                if s not in words_dict[word][1]:
+                    words_dict[word][1][s] = 1
+                elif s in words_dict[word][1]:
+                    words_dict[word][1][s] += 1
+    return words_dict
 
 
 # returns word occurrence count
-def occ(word: str, bible_dict: {}) -> int:
+def get_occ(word: str, bible_dict: {}) -> int:
     return bible_dict[word][0]
+
+
+def get_strongs(word: str, bible_dict: {}) -> str:
+    return bible_dict[word][1]
 
 
 def total_strongs(word: str, strongs_dict: {}) -> int:
@@ -131,6 +131,12 @@ def strongs_occ_per_word(word: str, strongs_dict: {}):
     # print(count)
 
 
+def get_keys(d: {}):
+    for key, value in d.items():
+        yield key
+        if isinstance(value, d):
+            yield from get_keys(value)
+
 # map strongs to words
 def strongs_dict():
     ...
@@ -139,8 +145,15 @@ def strongs_dict():
 if __name__ == '__main__':
     bible = read_bible('kjv_strongs.txt')
     # words_list = bible.lower().replace('.', '').replace(',', '').replace.split()
-    bible_dct = bible_to_dict(bible)
+
+    strip = string.punctuation.replace('{', '').replace('}', '')
+    cleaned_words = bible.lower().translate(str.maketrans('', '', strip)).split()
+    bible_dct = bible_to_dict(cleaned_words)
     print(bible_dct)
+
+    # Specifically has num of occurences of a word and num of occurences of Strongs
+    bible_dct_two = bible_to_dict_two(cleaned_words)
+    print(bible_dct_two)
 
     # print(words_dict)
     # print(total_strongs('god', words_dict))
