eb98563a5073a163a1c334428166c1ac743bcc48
zweatshirt <45150570+zweatshirt@users.noreply.github.com>
2023-12-23 22:59:33 -0600

    changes

--- main.py
+++ main.py
@@ -3,6 +3,20 @@ from bible_to_dicts.dict_one import DictOne
 from bible_to_dicts.dict_two import DictTwo
 from file_reading.vocab_dictionary import append_api_data, read_json_file
 
+# with vocab dictionary, create Strong's Concordance to map to verses
+
+
+def getpath(nested_dict, value, prepath=()):
+    for k, v in nested_dict.items():
+        path = prepath + (k,)
+        if v == value: # found value
+            return path
+        elif hasattr(v, 'items'): # v is a dict
+            p = getpath(v, value, path) # recursive call
+            if p is not None:
+                return p
+
+
 if __name__ == '__main__':
     # read the bible and store as string
     bible = read_bible('kjv_strongs.txt')
@@ -22,21 +36,12 @@ if __name__ == '__main__':
     print(b['genesis'][50])
     print(b.num_chapters(book := b['revelation']))
 
-
     # fix
     # print(b_two.get_word_strongs_tuple_for(last))
 
     # List of JSON of as many words in the bible as possible with definitions
     f_name = '/Users/zachjlinscott/PycharmProjects/bible_parser/dictionary.json'
-
-    # def find_paths(nested_dict, value, prepath=()):
-    #     for k, v in nested_dict.items():
-    #         path = prepath + (k,)
-    #         if v == value: # found value
-    #             yield path
-    #         elif hasattr(v, 'items'): # v is a dict
-    #             yield from find_paths(v, value, path)
-    #
-    # print(*find_paths(dictionary, 'image/svg+xml'))
-
     dictionary_json = read_json_file(f_name)
+
+
+
