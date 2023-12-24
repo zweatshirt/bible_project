01da2905e18716fa70ed64b89055a861b9cc686b
zweatshirt <45150570+zweatshirt@users.noreply.github.com>
2023-12-24 16:50:18 -0600

    refactor yet again. Also added a function to determine the memory usage of the app. Currently around .4GB. Will possibly do server API calls to a JSON of the dicts. This may decrease access/querying time but will massively save storage space for users.

--- main.py
+++ main.py
@@ -2,24 +2,18 @@ from file_reading.bible_reading import *
 from bible_to_dicts.dict_one import DictOne
 from bible_to_dicts.dict_two import DictTwo
 from file_reading.vocab_dictionary import append_api_data, read_json_file
+import os, psutil
 
-# with vocab dictionary, create Strong's Concordance to map to verses
 
-
-def getpath(nested_dict, value, prepath=()):
-    for k, v in nested_dict.items():
-        path = prepath + (k,)
-        if v == value: # found value
-            return path
-        elif hasattr(v, 'items'): # v is a dict
-            p = getpath(v, value, path) # recursive call
-            if p is not None:
-                return p
+def process_mem_size(p: psutil.Process()):
+    print('Memory usage in bytes: {}'.format(mem_bytes := p.memory_info().rss))
+    print('Memory usage in MB: {}'.format(megabytes := mem_bytes / (1000 ** 2)))
+    print('Memory usage in GB: {}'.format(gigabytes := megabytes / 1000))
 
 
 if __name__ == '__main__':
     # read the bible and store as string
-    bible = read_bible('kjv_strongs.txt')
+    bible = read_bible('data_files/kjv_strongs.txt')
 
     # list of each word in the bible
     # lower cased and rid of punctuation
@@ -42,8 +36,21 @@ if __name__ == '__main__':
     # print(b_two.get_word_strongs_tuple_for(last))
 
     # List of JSON of as many words in the bible as possible with definitions
-    f_name = '/Users/zachjlinscott/PycharmProjects/bible_parser/dictionary.json'
+    f_name = 'data_files/dictionary.json'
     dictionary_json = read_json_file(f_name)
 
+    process = psutil.Process()
+    process_mem_size(process)
 
 
+# with vocab dictionary, create Strong's Concordance to map to verses
+# not working, I'm not sure why
+# def getpath(nested_dict, value, prepath=()):
+#     for k, v in nested_dict.items():
+#         path = prepath + (k,)
+#         if v == value:  # found value
+#             return path
+#         elif hasattr(v, 'items'):  # v is a dict
+#             p = getpath(v, value, path)  # recursive call
+#             if p is not None:
+#                 return p
