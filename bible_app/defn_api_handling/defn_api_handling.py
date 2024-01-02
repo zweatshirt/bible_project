e250c01f9557f5e41017b9d0f5aa63b00fc75e2d
zachery with an e <45150570+zweatshirt@users.noreply.github.com>
2024-01-02 02:10:35 -0600

    Update defn_api_handling.py

--- bible_app/defn_api_handling/defn_api_handling.py
+++ bible_app/defn_api_handling/defn_api_handling.py
@@ -64,13 +64,18 @@ def read_def_json_f(f_name) -> []:
         return [loads(line) for line in f if line.strip()]
 
 
-def compare_definitions_to_dict(b_two: {}):
+def compare_definitions_to_dict(file, b_two: {}):
     # dictionary_json = read_def_json_f(DEFINITIONS_JSON_FILE)
-    unreadable = read_file(LEFTOVER_WORDS_FILE)
+    f = read_file(file)
+    
+    # if file == DEFINITIONNS_JSON_FILE:  # implement
 
-    for i in unreadable.split():
+    if file == LEFTOVER_WORDS_FILE:
+        for i in f.split():
         if i not in b_two.keys():
             print(i)
+    
+
 
 
 def delete_leftover_duplicates():
